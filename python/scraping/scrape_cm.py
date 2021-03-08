2f9161033a49efe2259a41896fd7324985bbd098
andrewfelton <andy.felton@gmail.com>
2021-03-07 22:14:27 -0500

    Updating CM scraping

--- python/scraping/scrape_cm.py
+++ python/scraping/scrape_cm.py
@@ -1,78 +1,120 @@
-import sys
-sys.path.append('/Users/andrewfelton/Documents/bb/2021/python')
-import selenium_utilities
-import time
-from bs4 import BeautifulSoup
-import pandas as pd
-import gspread
-#from df2gspread import df2gspread as d2g
-import gspread_dataframe
+def scrape_cm_draft(draft_num, gs=None, db=None):
 
-driver = selenium_utilities.start_driver()
-gc = gspread.service_account(filename='./bb-2021-2b810d2e3d25.json')
+    import sys
+    sys.path.append('python/utilities')
+    import selenium_utilities
+    import postgres
+    sys.path.append('python/munging')
+    import player_names
+    import time
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    import gspread
+    import gspread_dataframe
+    import re
 
-#draft_num = '46108' # Testing
-draft_num = '46117' # SoS Mock
-draft_url = "https://www.couchmanagers.com/mock_drafts/draft_chart.php?draftnum="+draft_num
-driver.get(draft_url)
-time.sleep(2)
-print('Arrived at '+driver.current_url)
+    driver = selenium_utilities.start_driver()
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-table = soup.find('table', 'all_teams_table')
+    #draft_num = '46233' # Testing
+    #draft_num = '46117' # SoS Mock
+    #draft_num = '46233' # SoS D2
+    #draft_num = '46234' # SoS D2
+    draft_url = "https://www.couchmanagers.com/mock_drafts/draft_chart.php?draftnum="+str(draft_num)
+    draft_url = "https://www.couchmanagers.com/mock_drafts/csv/download.php?draftnum="+str(draft_num)
 
-table_rows = table.find_all('tr')
+    driver.get(draft_url)
+    time.sleep(2)
+    print('Arrived at '+driver.current_url)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    selenium_utilities.stop_selenium('bbsel')
 
-
-teamnames = []
-tds = table_rows[0].find_all('td')
-for td in tds[1:]:
-    teamnames.append(td.text.strip())
-
-
-mockdraft = []
-round = 0
-for tr in table_rows[1:]:
-    round += 1
-    tds = tr.find_all('td')
-    row = [td.get_text(separator=' ') for td in tds[1:]]
-    print('Round 3: ' + str(row))
-    mockdraft.append(row)
-
-mock = pd.DataFrame(mockdraft, columns=teamnames)
-
-teams = pd.DataFrame(mock.columns, columns=['Team'])
-teams['Order'] = teams.reset_index().index
+    #csv_path = "/Users/andrewfelton/Downloads/docker/FanGraphs\ Leaderboard.csv"
 
 
-#mock.to_csv('./data/couchmanagers/mockdraft_'+draft_num+'.csv')
+    table = soup.find('table', 'all_teams_table')
+    table_rows = table.find_all('tr')
 
-mock_long = mock.unstack().reset_index()
-#mock_long.to_csv('./data/couchmanagers/mockdraft_'+draft_num+'.csv')
-mock_long.columns = ['Team', 'Round', 'Player']
+    teamnames = []
+    tds = table_rows[0].find_all('td')
+    for td in tds[1:]:
+        teamnames.append(td.text.strip())
+
+    mockdraft = []
+    round = 0
+    for tr in table_rows[1:]:
+        round += 1
+        tds = tr.find_all('td')
+        row = [td.get_text(separator=' ').strip() for td in tds[1:]]
+        row = [re.sub('\s+', ' ', td) for td in row]
+        print('Round ' + str(round) + ': ' + str(row))
+        if (teamnames!=row):
+            mockdraft.append(row)
+
+    mock = pd.DataFrame(mockdraft, columns=teamnames)
+
+    teams = pd.DataFrame(mock.columns, columns=['Team'])
+    teams['Order'] = teams.reset_index().index + 1
 
 
-mock_long = mock_long.merge(
-    teams,
-    how='outer',
-    on='Team'
-)
+    #mock.to_csv('./data/couchmanagers/mockdraft_'+draft_num+'.csv')
+    mock_long = mock.unstack().reset_index()
+    #mock_long.to_csv('./data/couchmanagers/mockdraft_'+draft_num+'.csv')
+    mock_long.columns = ['Team', 'Round', 'Player']
 
-mock_long['Pick'] = len(teams) * mock_long['Round'] + mock_long['Order'] + 1
-mock_long = mock_long.sort_values(by='Pick')
-mock_long['Round'] = mock_long['Round']+1
+    mock_long = mock_long.merge(
+        teams,
+        how='outer',
+        on='Team'
+    )
 
-
-bb2021 = gc.open("BB 2021")
-sheettitle = "Mock "+draft_num
-
-bb2021.values_clear(sheettitle + "!A:Z")
-gspread_dataframe.set_with_dataframe(bb2021.worksheet(sheettitle), mock_long)
+    mock_long['Pick'] = (len(teams) * mock_long['Round']) + mock_long['Order']
+    mock_long = mock_long.sort_values(by='Pick')
+    mock_long['Round'] = mock_long['Round']+1
 
 
-combined = bb2021.worksheet('Combined')
-hitter_projections = bb2021.worksheet('Hitter Projections')
+    names = player_names.get_player_names()
 
-combined.update
 
-selenium_utilities.stop_selenium('bbsel')
+
+
+
+    if (gs!=None):
+        gc = gspread.service_account(filename='./bb-2021-2b810d2e3d25.json')
+        bb2021 = gc.open("BB 2021")
+        sheettitle = "Mock "+draft_num
+        bb2021.values_clear(sheettitle + "!A:Z")
+        gspread_dataframe.set_with_dataframe(bb2021.worksheet(sheettitle), mock_long)
+        combined = bb2021.worksheet('Combined')
+        hitter_projections = bb2021.worksheet('Hitter Projections')
+        combined.update
+        hitter_projections.update
+
+    if (db!=None):
+        bbdb = postgres.connect_to_bbdb()
+        mock_long.to_sql('cm_mock_'+draft_num, bbdb, schema='drafts', if_exists='replace')
+
+
+
+
+#scrape_cm_draft(draft_num='46233', db=True)
+
+
+
+import urllib3
+
+http = urllib3.PoolManager()
+
+filepath = '~/Downloads/cm_draft_'+draft_num+'.csv'
+
+import requests
+import csv
+r = requests.get(draft_url).text.splitlines()
+
+
+mock = []
+for line in r:
+    pick = line.split(',')
+    for i in range(0, len(pick)):
+        pick[i] = pick[i].replace('"','')
+    mock.append(pick)
+mock = pd.DataFrame(mock)
