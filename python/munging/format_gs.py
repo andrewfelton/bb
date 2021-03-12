010f7cf0c698d2d20c7a0adc42a053c8d49badec
andrewfelton <andy.felton@gmail.com>
2021-03-11 21:18:08 -0500

    Set up GS formatting

--- python/munging/format_gs.py
+++ python/munging/format_gs.py
@@ -1,14 +1,30 @@
-import gspread
-import gspread_formatting as gsfmt
+def format_gs_hitting():
+    import gspread
+    import gspread_formatting as gsfmt
 
-gc = gspread.service_account(filename='./bb-2021-2b810d2e3d25.json')
-bb2021 = gc.open("BB 2021")
+    gc = gspread.service_account(filename='./bb-2021-2b810d2e3d25.json')
+    bb2021 = gc.open("BB 2021 SoS")
 
+    rate = gsfmt.CellFormat(numberFormat=gsfmt.NumberFormat(type='NUMBER', pattern='0.000'))
+    cardinal = gsfmt.CellFormat(numberFormat=gsfmt.NumberFormat(type='NUMBER', pattern='0'))
+    value = gsfmt.CellFormat(numberFormat=gsfmt.NumberFormat(type='NUMBER', pattern='0.0'))
 
-fmt = gsfmt.NumberFormat(
-    type='NUMBER',
-    pattern='0.000'
-)
+    gsfmt.format_cell_range(bb2021.worksheet('Hitter Projections'), 'C:G', cardinal)
+    gsfmt.format_cell_range(bb2021.worksheet('Hitter Projections'), 'H:I', rate)
+    gsfmt.format_cell_range(bb2021.worksheet('Hitter Projections'), 'J:K', value)
 
-gsfmt.format_cell_range(bb2021.worksheet('Hitter Projections'), 'I:J', fmt)
+def format_gs_pitching():
+    import gspread
+    import gspread_formatting as gsfmt
+
+    gc = gspread.service_account(filename='./bb-2021-2b810d2e3d25.json')
+    bb2021 = gc.open("BB 2021 SoS")
+
+    rate = gsfmt.CellFormat(numberFormat=gsfmt.NumberFormat(type='NUMBER', pattern='0.00'))
+    cardinal = gsfmt.CellFormat(numberFormat=gsfmt.NumberFormat(type='NUMBER', pattern='0'))
+    value = gsfmt.CellFormat(numberFormat=gsfmt.NumberFormat(type='NUMBER', pattern='0.0'))
+
+    gsfmt.format_cell_range(bb2021.worksheet('Pitcher Projections'), 'C:G', cardinal)
+    gsfmt.format_cell_range(bb2021.worksheet('Pitcher Projections'), 'H:I', rate)
+    gsfmt.format_cell_range(bb2021.worksheet('Pitcher Projections'), 'J:K', value)
 
