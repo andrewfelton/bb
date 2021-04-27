ba272d982ca3acb7b7af573caf1d14abac022fb0
andrewfelton <andy.felton@gmail.com>
2021-04-26 21:54:05 -0400

    Updated to break out selenium into browser types

--- python/general/selenium_utilities.py
+++ python/general/selenium_utilities.py
@@ -16,12 +16,36 @@ def start_selenium():
 
 
 def create_selenium_container():
+    return create_selenium_container_chrome()
+
+
+def create_selenium_container_chrome():
+    import os
+    
     commands = [
         "docker",
         "run -d",
         "-p 4445:4444",
         "-p 5913:5900",
-        "--name bbsel"
+        "--name bbsel",
+        "-v ${HOME}/Downloads/docker/:/home/seluser/Downloads/",
+        "selenium/standalone-chrome",
+    ]
+    # '  '.join(commands)
+    stream_docker = os.popen(' '.join(commands))
+    docker_ff_id = stream_docker.read().rstrip()
+
+    print('Started docker with id '+docker_ff_id)
+    return docker_ff_id
+
+
+def create_selenium_container_ff():
+    commands = [
+        "docker",
+        "run -d",
+        "-p 4445:4444",
+        "-p 5913:5900",
+        "--name bbsel",
         "-v ${HOME}/Downloads/docker/:/home/seluser/Downloads/",
     #    "selenium/standalone-firefox",
         "selenium/standalone-firefox-debug",
@@ -39,7 +63,7 @@ def stop_selenium(docker_id):
     #os.system('docker rm ' + docker_id)
 
 
-def start_driver():
+def start_driver_ff():
     import sys
     sys.path.append('/python')
     import selenium_utilities
@@ -70,4 +94,58 @@ def start_driver():
         browser_profile=profile
         )
     return driver
+
+
+
+def start_driver_chrome():
+    import sys
+    sys.path.append('/python')
+    import selenium_utilities
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    import time
+
+    # start a selenium container
+    docker_ff_id = selenium_utilities.start_selenium()
+    time.sleep(5)
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.set_headless = True
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-logging')
+    chrome_options.add_argument('--start-maximized') # open Browser in maximized mode
+    chrome_options.add_argument('--disable-infobars') # disabling infobars
+    chrome_options.add_argument('--disable-dev-shm-usage') # overcome limited resource problems
+    chrome_options.add_argument('--disable-setuid-sandbox')
+    chrome_options.add_argument('--disable-extensions')
+
+    driver = webdriver.Remote(
+        command_executor="http://127.0.0.1:4445/wd/hub",
+        options=chrome_options
+    )
+
+    driver = webdriver.Remote(
+        command_executor="http://127.0.0.1:4445/wd/hub",
+        desired_capabilities=chrome_options.to_capabilities()
+    )
+    return driver
+    driver.close()
+
+
+def start_driver_local_chrome():
+    from selenium import webdriver
+    chrome_options = webdriver.ChromeOptions()
+    #chrome_options.add_argument('--headless')
+    prefs = {"profile.default_content_settings.popups": 0,
+                "download.default_directory": r"/Users/andrewfelton/Downloads/docker/",
+                "directory_upgrade": True}
+    chrome_options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(desired_capabilities=chrome_options.to_capabilities())
+    return driver
+
+
+def start_driver():
+    return start_driver_local_chrome()
+
 
