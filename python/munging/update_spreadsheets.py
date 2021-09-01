1d5614f504796b731af1bf4d4dfec2e3b91adb66
andrewfelton <andy.felton@gmail.com>
2021-09-01 16:24:33 -0400

    Clean up

--- python/munging/update_spreadsheets.py
+++ python/munging/update_spreadsheets.py
@@ -39,28 +39,36 @@ def inseason_standings_sos():
 
 
 def update_relievers_last14():
+    import pandas as pd
+    import gspread
+    import gspread_dataframe as gsdf
+
+    from general import postgres
+    from general import gs
+    
     bbdb = postgres.connect_to_bbdb()
     relievers_last14 = pd.read_sql('''
         SELECT 
-            r."Name", r."Team", ff_elig.ff_elig, r."G", r."IP", r."SV",
-            r."HLD", r."gmLI", r."WPA", r."ERA", r."kwERA", 
-            r."xFIP", r."SIERA", r."xERA", r."CSW_pct", r."K_pct", 
-            r."BB_pct", r."SwStr_pct", r."vFA", r."BABIP", r."LOB_pct", 
-            r."HR_FB", r.asof_date, r.fg_id,
+            r.name, r.team, ff_elig.ff_elig, r.g, r.ip, r.sv,
+            r.hld, r.gmli, r.wpa, r.era, r.kwera, 
+            r.xfip, r.siera, r.xera, r.csw_pct, r.k_pct, 
+            r.bb_pct, r.swstr_pct, r.vfa, r.babip, r.lob_pct, 
+            r.hr_fb, r.asof_date, r.fg_id,
             rosters_sos."Team" as sos_team,
             rosters_legacy."Team" as legacy_team
-        FROM tracking.relievers_last14_raw r
+        FROM tracking.relievers_last14 r
         LEFT JOIN reference.player_pool_ff ff_elig ON r.fg_id=ff_elig.fg_id
         LEFT JOIN rosters.sos rosters_sos ON r.fg_id=rosters_sos.fg_id
         LEFT JOIN rosters.legacy rosters_legacy ON r.fg_id=rosters_legacy.fg_id
         WHERE ff_elig IN ('P', 'RP', 'SP') OR ff_elig IS NULL
-        ORDER BY r."WPA" DESC
+        ORDER BY r.wpa DESC
         ''', con=bbdb)
-    gc = gspread.service_account(filename='./bb-2021-2b810d2e3d25.json')
+    gc = gspread.service_account(filename='../bb-2021-2b810d2e3d25.json')
     bb2021 = gc.open("BB 2021 InSeason")
     sheettitle = "Relievers - Last 14"
     bb2021.values_clear(sheettitle + "!A:Z")
     gsdf.set_with_dataframe(bb2021.worksheet(sheettitle), relievers_last14)
+    gs.format_gsheet(bb2021.worksheet(sheettitle))
 
     
 
