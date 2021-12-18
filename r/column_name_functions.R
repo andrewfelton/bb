5e9c616358709f71989bbd80d065f6d325a603eb
andrewfelton <andy.felton@gmail.com>
2021-12-18 18:17:39 -0500

    Removed R code

--- r/column_name_functions.R
+++
@@ -1,9 +0,0 @@
-
-
-get_suffixed_names <- function(colnames, suffix) {
-  newnames = colnames
-  for(i in 1:length(colnames)) {
-    newnames[i] = paste(colnames[i], ".", suffix, sep='')
-  }
-  return(newnames)
-}
